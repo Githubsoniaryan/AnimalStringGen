9303cc67c4d64cc385f08a959418db357e99c8db
SoniAryanXmart <154493242+Githubsoniaryan@users.noreply.github.com>
2024-02-06 10:32:56 +0530

    Update config.py

--- config.py
+++ config.py
@@ -12,6 +12,6 @@ BOT_TOKEN = getenv("BOT_TOKEN")
 MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 
 OWNER_ID = int(getenv("OWNER_ID", 1356469075))
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/royalgroupop")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/knight_society")
 
 
